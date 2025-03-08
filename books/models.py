97bc8e644aaaf6e6c9b894e8d300f528364a09a2
kkkkkkkkatya <vasyabarilyuk@gmail.com>
2025-03-08 19:32:08 +0200

    Add the book model and registered it in the admin panel

--- books/models.py
+++ books/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from decimal import Decimal
 
-# Create your models here.
+
+class Book(models.Model):
+    COVER_CHOICES = [
+        ("HARD", "Hardcover"),
+        ("SOFT", "Softcover"),
+    ]
+
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    cover = models.CharField(max_length=10, choices=COVER_CHOICES)
+    inventory = models.PositiveIntegerField()
+    daily_fee = models.DecimalField(max_digits=6, decimal_places=2, default=Decimal("0.00"))
+
+    def __str__(self):
+        return f"{self.title} by {self.author}"
+
