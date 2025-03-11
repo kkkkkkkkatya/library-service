5cadf643c6db13be7c76643eebacb875974011c4
kkkkkkkkatya <vasyabarilyuk@gmail.com>
2025-03-11 21:38:31 +0200

    Implement the list & detail endpoints

--- borrowings/views.py
+++ borrowings/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from borrowings.models import Borrowing
+from borrowings.serializers import BorrowingReadSerializer
 
-# Create your views here.
+
+class BorrowingViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Borrowing.objects.select_related("book", "user").all()
+    serializer_class = BorrowingReadSerializer
+    permission_classes = [permissions.IsAdminUser]
