775cb1bc6697ec980f886f86638b74d20e6d419f
kkkkkkkkatya <vasyabarilyuk@gmail.com>
2025-03-12 16:09:58 +0200

    Add validation method, decrease inventory by 1 for book and add current user to the borrowing

--- borrowings/models.py
+++ borrowings/models.py
@@ -1,8 +1,7 @@
+from django.core.exceptions import ValidationError
 from django.db import models
 from django.conf import settings
-
 from books.models import Book
-
 
 class Borrowing(models.Model):
     borrow_date = models.DateField(auto_now_add=True)
@@ -23,6 +22,25 @@ class Borrowing(models.Model):
                 name="check_actual_return_after_borrow",
             ),
         ]
+
+    @staticmethod
+    def validate_borrowing(book, error_to_raise):
+        """Ensures book inventory is not 0 before borrowing."""
+        if book.inventory <= 0:
+            raise error_to_raise(
+                {"book": f"Book '{book.title}' is out of stock and cannot be borrowed."}
+            )
+
+    def clean(self):
+        """Runs validation before saving."""
+        Borrowing.validate_borrowing(self.book, ValidationError)
+
+    def save(self, *args, **kwargs):
+        """Validates, decreases inventory, and saves the borrowing."""
+        self.full_clean()
+        self.book.inventory -= 1
+        self.book.save(update_fields=["inventory"])
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return f"{self.user} borrowed {self.book} on {self.borrow_date}"
--- borrowings/serializers.py
+++ borrowings/serializers.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import ValidationError
 from rest_framework import serializers
 from borrowings.models import Borrowing
 from books.serializers import BookSerializer
@@ -12,7 +13,21 @@ class BorrowingReadSerializer(serializers.ModelSerializer):
         read_only_fields = ["id", "borrow_date", "user"]
 
 
-class BorrowingWriteSerializer(serializers.ModelSerializer):
+class BorrowingCreateSerializer(serializers.ModelSerializer):
+    """Serializer for creating borrowings with inventory validation."""
+
     class Meta:
         model = Borrowing
-        fields = ["id", "borrow_date", "expected_return_date", "actual_return_date","book"]
+        fields = ["id", "expected_return_date", "book"]
+
+    def validate(self, attrs):
+        """Validate borrowing using the model method before saving."""
+        data = super().validate(attrs)
+        Borrowing.validate_borrowing(attrs["book"], ValidationError)
+        return data
+
+    def create(self, validated_data):
+        """Attach the current user and create a borrowing after validation."""
+        user = self.context["request"].user
+        borrowing = Borrowing.objects.create(user=user, **validated_data)
+        return borrowing
